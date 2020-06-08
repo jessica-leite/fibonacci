bd86ea7c78f9b9d3ce5d7f2d173e8a0e114884a4
Jessica Leite <jessica.mleite@gmail.com>
2020-06-08 17:08:50 +0000

    Sequence method created

---
+++ Numbers.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Fibonacci
+{
+    public class Numbers
+    {
+        public static List<int> Fibonacci()
+        {
+            int previousNumber = 0, currentNumber = 1;
+
+            var numbers = new List<int> { previousNumber, currentNumber };
+
+            while (previousNumber + currentNumber <= 350)
+            {
+                var newNumber = previousNumber + currentNumber;
+
+                numbers.Add(newNumber);
+
+                previousNumber = currentNumber;
+                currentNumber = newNumber;
+            }
+
+            return numbers;
+        }
+
+        public static bool IsFibonacci(int number)
+        {
+            var sequence = Fibonacci();
+
+            return sequence.Contains(number);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var fibonacci = Numbers.Fibonacci();
+
+            fibonacci.ForEach(number => Console.WriteLine(number));
+
+            Console.ReadLine();
+
+            var isFibonacci = Numbers.IsFibonacci(55);
+            var isFibonacci2 = Numbers.IsFibonacci(200);
+
+            return;
         }
     }
 }
